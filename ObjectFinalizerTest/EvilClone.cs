794b824f3b457155eb015fa19441a4699bd0e660
Bartek <bartlomiej.kacki@gmail.com>
2024-11-23 20:01:13 +0100

    Add project files.

---
+++ ObjectFinalizerTest/EvilClone.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectFinalizerTest
+{
+    public class EvilClone
+    {
+        public static int CloneCount = 0;
+        public int CloneID { get; } = ++CloneCount;
+
+        public EvilClone() => Console.WriteLine("Klon nr {0} sieje zniszczenie.", CloneID);
+
+        //finalizer
+        ~EvilClone() => Console.WriteLine("Klon nr {0} został usunięty.", CloneID);
+    }
+}
---
+++ ObjectFinalizerTest/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ObjectFinalizerTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+            var evilClones = new List<EvilClone>();
+
+            while (true)
+            {
+                switch (Console.ReadKey(true).KeyChar)
+                {
+                    case 'a':
+                        evilClones.Add(new EvilClone());
+                        break;
+                    case 'c':
+                        Console.WriteLine("Opróżnienie listy, czas: {0}", stopwatch.ElapsedMilliseconds);
+                        evilClones.Clear();
+                        break;
+                    case 'g':
+                        Console.WriteLine("Odzyskanie pamięci, czas: {0}", stopwatch.ElapsedMilliseconds);
+                        GC.Collect();
+                        break;
+                }
+            }
+        }
+    }
+}
